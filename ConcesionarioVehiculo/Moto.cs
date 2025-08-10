0b33357ba756b775072bc6c50767bee45cfe7c50
Beemil <valeskavr17@gmail.com>
2025-08-09 21:59:27 -0600

    Clase moto con condicional para maletero

--- ConcesionarioVehiculo/Moto.cs
+++ ConcesionarioVehiculo/Moto.cs
@@ -1,12 +1,33 @@
 ﻿
 namespace ConcesionarioVehiculo
 {
-    public class Moto
+    public class Moto : Vehiculo
     {
         public int Cilindrada { get; set; }
         public bool TieneMaletero { get; set; }
 
+        public Moto(){ }
+        public Moto(string marca, string modelo, int año, int cilindrada, bool tieneMaletero) : base(marca, modelo, año)
+        {
+            Cilindrada = cilindrada;
+            TieneMaletero = tieneMaletero;
+        }
+
         public override void MostrarInformacion()
+        {
+            base.MostrarInformacion();
+            Console.WriteLine($"Cilindrada: {Cilindrada} cc");
+            
+            if (TieneMaletero)
+            {
+                Console.WriteLine("Tiene maletero: Si");
+            }
+            else
+            {
+                Console.WriteLine("No tiene maletero: No");
+            }
+            Console.WriteLine();
+        }
 
     }
 }
